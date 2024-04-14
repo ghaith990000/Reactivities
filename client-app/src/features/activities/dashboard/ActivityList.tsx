efdf3b36c2a0d014c242837ec15bfd8b5312e7d2
ghaith990000 <ghaith990000@gmail.com>
2024-04-15 00:39:40 +0300

    added grouped activities by date

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,22 +1,34 @@
-import { Item, Segment } from "semantic-ui-react";
+import { Header, Item, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import ActivityListItem from "./ActivityListItem";
+import { Fragment } from "react/jsx-runtime";
 
 
 const ActivityList = () => {
     const {activityStore} = useStore();
-    const { activitiesByDate} = activityStore;
+    const { groupedActivities} = activityStore;
     
 
   return (
-    <Segment>
-        <Item.Group divided>
-            {activitiesByDate.map(activity => (
-                <ActivityListItem activity={activity} />
-            ))}
-        </Item.Group>
-    </Segment>
+    <>
+        {groupedActivities.map(([group, activities]) => (
+             <Fragment key={group}>
+                <Header sub color='teal'>
+                    {group}
+                </Header>
+                <Segment>
+                    <Item.Group divided>
+                        {activities.map(activity => (
+                            <ActivityListItem activity={activity} />
+                        ))}
+                    </Item.Group>
+                </Segment>
+             </Fragment>
+        ))}
+        
+    
+    </>
   )
 }
 
