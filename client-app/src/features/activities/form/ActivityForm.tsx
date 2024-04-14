c5e3e98ae2997b8f49e34502207f4a28e4ef99eb
ghaith990000 <ghaith990000@gmail.com>
2024-04-14 20:34:59 +0300

    configure react-router-dom package to manage routing in the application

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,14 +1,18 @@
 import { Button, Form, Segment } from "semantic-ui-react"
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
+import { useParams } from "react-router-dom";
+import { Activity } from "../../../app/models/activity";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 
 const ActivityForm = () => {
     const {activityStore} = useStore();
-    const {selectedActivity, closeForm, createActivity, updateActivity, loading} = activityStore;
+    const {createActivity, updateActivity, loading, loadActivity, loadingInitial} = activityStore;
+    const {id} = useParams();
 
-    const initialState = selectedActivity ?? {
+    const [activity, setActivity] = useState<Activity>({
         id: '',
         title: '',
         category: '',
@@ -16,7 +20,12 @@ const ActivityForm = () => {
         date: '',
         city: '',
         venue: '',
-    }
+    });
+
+    useEffect(()=>{
+        if(id) loadActivity(id).then(activity => setActivity(activity!))
+    }, [id, loadActivity])
+
 
     function handleSubmit(){
         activity.id ? updateActivity(activity) : createActivity(activity);
@@ -28,7 +37,7 @@ const ActivityForm = () => {
         setActivity({...activity, [name]: value});
     }
 
-    const [activity, setActivity] = useState(initialState)
+    if(loadingInitial) return <LoadingComponent content='Loading activity...' />
   return (
     <Segment clearing>
         <Form onSubmit={handleSubmit} autoComplete='off'>
@@ -39,7 +48,7 @@ const ActivityForm = () => {
             <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
             <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
             <Button loading={loading} floated="right" positive type='submit' content="Submit" />
-            <Button onClick={closeForm} floated="right" type='button' content="Cancel" />
+            <Button floated="right" type='button' content="Cancel" />
         </Form>
     </Segment>
   )
---
+++ client-app/src/features/home/HomePage.tsx
@@ -0,0 +1,11 @@
+import { Container } from "semantic-ui-react"
+
+const HomePage = () => {
+  return (
+    <Container style={{marginTop: '7em'}}>
+        <h1>Home Page</h1>
+    </Container>
+  )
+}
+
+export default HomePage
