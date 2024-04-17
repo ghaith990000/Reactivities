d94801e495e6939097f65adb04f3bd5ffea9b1fa
ghaith990000 <ghaith990000@gmail.com>
2024-04-17 23:41:51 +0300

    create MyTextArea component with useField from Formik

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,8 +6,10 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import { Activity } from "../../../app/models/activity";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import {v4 as uuid} from 'uuid'; 
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, ErrorMessage } from "formik";
 import * as Yup from 'yup';
+import MyTextInput from "../../../app/common/form/MyTextInput";
+import MyTextArea from "../../../app/common/form/MyTextArea";
 
 const ActivityForm = () => {
     const {activityStore} = useStore();
@@ -26,7 +28,13 @@ const ActivityForm = () => {
     });
 
     const validationSchema = Yup.object({
-        title: Yup.string().required('The activity title is required')
+        title: Yup.string().required('The activity title is required'),
+        description: Yup.string().required('The activity description is required'),
+        category: Yup.string().required(),
+        date: Yup.string().required(),
+        venue: Yup.string().required(),
+        city: Yup.string().required(),
+
     })
     useEffect(()=>{
         if(id) loadActivity(id).then(activity => setActivity(activity!))
@@ -57,16 +65,12 @@ const ActivityForm = () => {
             onSubmit={values => console.log(values)}>
             {({handleSubmit}) =>(
                 <Form className="ui form" onSubmit={handleSubmit} autoComplete='off'>
-                    <FormField>
-                        <Field placeholder='Title' name='title'/>
-                        <ErrorMessage name='title' 
-                            render={error => <Label basic color='red' content={error} />} />
-                    </FormField>
-                    <Field placeholder='Description' name='description' />
-                    <Field placeholder='Category' name='category' />
-                    <Field type='date' placeholder='Date' name='date' />
-                    <Field placeholder='City' name='city' />
-                    <Field placeholder='Venue' name='venue' />
+                    <MyTextInput name='title' placeholder="Title" />
+                    <MyTextArea rows={3} placeholder='Description' name='description' />
+                    <MyTextInput placeholder='Category' name='category' />
+                    <MyTextInput placeholder='Date' name='date' />
+                    <MyTextInput placeholder='City' name='city' />
+                    <MyTextInput placeholder='Venue' name='venue' />
                     <Button loading={loading} floated="right" positive type='submit' content="Submit" />
                     <Button as={Link} to='/activities' floated="right" type='button' content="Cancel" />
                 </Form>
