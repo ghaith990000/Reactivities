120fce07cc60435fa67c78720a147e4f7258d43e
ghaith990000 <ghaith990000@gmail.com>
2024-04-19 19:10:39 +0300

    login using the form after filling the credentials

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,22 +1,25 @@
 import { Form, Formik } from "formik"
 import MyTextInput from "../../app/common/form/MyTextInput"
 import { Button } from "semantic-ui-react"
+import { useStore } from "../../app/stores/store"
+import { observer } from "mobx-react-lite"
 
 const LoginForm = () => {
+    const {userStore} = useStore();
   return (
     <Formik
         initialValues={{email: '', password: ''}}
-        onSubmit={values => console.log(values)}
+        onSubmit={values => userStore.login(values)}
     >
-        {({handleSubmit})=>(
+        {({handleSubmit, isSubmitting})=>(
             <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
                 <MyTextInput placeholder="Email" name="email" />
                 <MyTextInput placeholder="Password" name="password" type='password' />
-                <Button positive content='Login' type='submit' fluid />
+                <Button positive loading={isSubmitting} content='Login' type='submit' fluid />
             </Form>
         )}
     </Formik>
   )
 }
 
-export default LoginForm
+export default observer(LoginForm);
